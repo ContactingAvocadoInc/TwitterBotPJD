d0af1819cf347b8008306303075387aefbe99e83
Joe Schofield <joe_schofield1@next.co.uk>
2022-11-26 20:16:34 +0000

    First project

---
+++ TwitterBotPJD.ReceiveTweets/Program.cs
@@ -0,0 +1,7 @@
+using TwitterBotPJD.ReceiveTweets;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services => { services.AddHostedService<Worker>(); })
+    .Build();
+
+await host.RunAsync();
